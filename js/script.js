fccdff81e4962d355a17b9892b4bff2c94acf7e8
Alexa_Chukina <alexachukina@gmail.com>
2021-03-28 02:06:21 +0300

    Change graph generate

--- js/script.js
+++ js/script.js
@@ -1,66 +1,68 @@
-let firstGraphParam = {
-    credits: {
-        enabled: false
-    },
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Динамика изменения состояния окружающей среды'
-    },
-    /*subtitle: {
-        text: 'Source: WorldClimate.com'
-    },*/
-    xAxis: {
-        categories: [
-            '2017',
-            '2018',
-            '2019',
-        ],
-        //crosshair: true,
-        gridLineWidth: 0.5,
-        gridLineColor: '#C0C0C0'
-    },
-    yAxis: {
-        gridLineColor: '#FF0000',
-        gridLineWidth:1,
-        min: 0,
-        title: {
-            text: 'Уровень загрязнения, %'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Атмосферный воздух',
-        color:'#99CCFF',
-        data: [49.9, 71.5, 90.4]
+function updateDataGraph( data=null ){
+  let firstGraphParam = {
+      credits: {
+          enabled: false
+      },
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'Динамика изменения состояния окружающей среды'
+      },
+      /*subtitle: {
+          text: 'Source: WorldClimate.com'
+      },*/
+      xAxis: {
+          categories: [
+              '2017',
+              '2018',
+              '2019',
+          ],
+          //crosshair: true,
+          gridLineWidth: 0.5,
+          gridLineColor: '#C0C0C0'
+      },
+      yAxis: {
+          gridLineColor: '#FF0000',
+          gridLineWidth:1,
+          min: 0,
+          title: {
+              text: 'Уровень загрязнения, %'
+          }
+      },
+      tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+              '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      series: [{
+          name: 'Атмосферный воздух',
+          color:'#99CCFF',
+          data: [49.9, 71.5, 90.4]
 
-    }, {
-        name: 'Водные объекты',
-        color:'#0066CC',
-        data: [83.6, 78.8, 98.5]
+      }, {
+          name: 'Водные объекты',
+          color:'#0066CC',
+          data: [83.6, 78.8, 98.5]
 
-    }]
-};
-
+      }]
+  };
+  return firstGraphParam;
+}
 
 
 
 $(document).ready(function(){
-  Highcharts.chart( 'first-graph', firstGraphParam );
+  Highcharts.chart( 'first-graph', updateDataGraph() );
   //Highcharts.chart( 'second-graph', firstGraphParam );
   //Highcharts.chart( 'third-graph', firstGraphParam );
 });
