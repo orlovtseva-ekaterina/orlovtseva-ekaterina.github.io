690063d590331751f9b36e54d3745b8215728e9f
vladimir-chukin <chukin@mail.ru>
2021-03-28 12:57:47 +0300

    final version

--- js/choropleth.js
+++ js/choropleth.js
@@ -32,13 +32,26 @@ var color = d3.scale.linear()
     .range(["red", "orange", "green"]);
 
 // добавление div для всплывающих подсказок
-var div = d3.select("#my_dataviz")
+var div1 = d3.select("#my_dataviz1")
   .append("div")
   .attr("class", "tooltip_d3")
   .style("opacity", 0);
 
 // добавляем SVG для карты
-var svg = d3.select("#my_dataviz")
+var svg1 = d3.select("#my_dataviz1")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("margin", "10px auto");
+
+// добавление div для всплывающих подсказок
+var div2 = d3.select("#my_dataviz2")
+  .append("div")
+  .attr("class", "tooltip_d3")
+  .style("opacity", 0);
+
+// добавляем SVG для карты
+var svg2 = d3.select("#my_dataviz2")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
@@ -84,14 +97,16 @@ function parseDataFromCSV(){
 // отображение картограммы
 function ready(error, map, data) {
 
-  var rateById = {};
+  var rateById_air = {};
+  var rateById_water = {};
   var nameById = {};
 
 
   // fill arrays by data from CSV file
   data.forEach( function(d) {
     if(d.water_total != 0) {
-      rateById[d.iso3166_alpha2] = Math.round(d.air_total*100);
+      rateById_air[d.iso3166_alpha2] = Math.round(d.air_total*100);
+      rateById_water[d.iso3166_alpha2] = Math.round(d.water_total*100);
     }
     nameById[d.iso3166_alpha2] = d.name_ru;
   });
@@ -102,7 +117,7 @@ function ready(error, map, data) {
   _Global_features = features;
 
   // добавление
-  svg.append("g")
+  svg1.append("g")
     .attr("class", "region")
     .selectAll("path")
     // .data(topojson.object(map, map.objects.russia).geometries)
@@ -112,12 +127,12 @@ function ready(error, map, data) {
     .attr("d", path)
     .style("fill", function(d) {
       // if data are missed then grey color
-      if(rateById[d.properties.ISO_2] == null) {
+      if(rateById_air[d.properties.ISO_2] == null) {
         return '#eeeeee';
       }
       // else use palette
       else {
-        return color(rateById[d.properties.ISO_2]);  // <<
+        return color(rateById_air[d.properties.ISO_2]);  // <<
       }
     })
     .style("opacity", 0.8)
@@ -125,23 +140,110 @@ function ready(error, map, data) {
   // add event listners
   .on("mouseover", function(d) {
     d3.select(this).transition().duration(300).style("opacity", 1);
-    div.transition().duration(300).style("opacity", 1)
+    div1.transition().duration(300).style("opacity", 1)
     // вывод значений из датасета в подсказку
     var level = 'Нет данных измерений';
-    if(rateById[d.properties.ISO_2]<33) {
+    if(rateById_air[d.properties.ISO_2]<33) {
       level = 'Низкий уровень загрязнения';
     }
-    if(rateById[d.properties.ISO_2]>=33 && rateById[d.properties.ISO_2]<67) {
+    if(rateById_air[d.properties.ISO_2]>=33 && rateById_air[d.properties.ISO_2]<67) {
       level = 'Повышенный уровень загрязнения';
     }
-    if(rateById[d.properties.ISO_2]>=67) {
+    if(rateById_air[d.properties.ISO_2]>=67) {
       level = 'Высокий уровень загрязнения';
     }
     var percent = '';
-    if(rateById[d.properties.ISO_2]>=0) {
-      percent = " ("+rateById[d.properties.ISO_2]+"%)";
+    if(rateById_air[d.properties.ISO_2]>=0) {
+      percent = " ("+rateById_air[d.properties.ISO_2]+"%)";
     }
-    div.html(nameById[d.properties.ISO_2] + "<br/>" + "<span style='font-size:16px;font-weight:700'>" + level + "</span>"+percent)
+    div1.html(nameById[d.properties.ISO_2] + "<br/>" + "<span style='font-size:16px;font-weight:700'>" + level + "</span>"+percent)
+      .style("left", (d3.event.layerX) + "px")
+      .style("top", (d3.event.layerY - 30) + "px");
+  })
+  .on("mouseout", function() {
+    d3.select(this)
+      .transition().duration(300)
+      .style("opacity", 0.7);
+    div1.transition().duration(300)
+      .style("opacity", 0);
+  })
+  .on("click", function(d) {
+    $('#overlay').fadeToggle('fast',function(){
+        $('#box').animate({'right':'0'},500);
+    });
+
+    $('#name_region_head').text( nameById[d.properties.ISO_2] );
+    Highcharts.chart( 'first-graph', updateDataGraph( nameById[d.properties.ISO_2], dataForGraphHighcharts[d.properties.ISO_2] ) );
+  })
+
+  // добавление городов на карту
+  d3.tsv("/data/cities.tsv", function(error, data) {
+    // создание массива данных о городах
+    var city = svg1.selectAll("g.city")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("class", "city")
+      .attr("transform", function(d) {
+        return "translate(" + projection([d.lon, d.lat]) + ")";
+      });
+    // добавление точек на карту
+    city.append("circle")
+      .attr("r", 2)
+      .style("fill", "red")
+      .style("opacity", 0.75);
+    // добавление названий городов на карту
+    city.append("text")
+      .attr("x", 5)
+      .text(function(d) {
+        return d.City;
+      });
+  });
+
+
+//////////////////////////////// WATER /////////////////////////
+
+  // добавление
+  svg2.append("g")
+    .attr("class", "region")
+    .selectAll("path")
+    // .data(topojson.object(map, map.objects.russia).geometries)
+    // .data(topojson.feature(map, map.objects.russia).features) //<-- in case topojson.v1.js
+    .data(features.features)
+    .enter().append("path")
+    .attr("d", path)
+    .style("fill", function(d) {
+      // if data are missed then grey color
+      if(rateById_water[d.properties.ISO_2] == null) {
+        return '#eeeeee';
+      }
+      // else use palette
+      else {
+        return color(rateById_water[d.properties.ISO_2]);  // <<
+      }
+    })
+    .style("opacity", 0.8)
+
+  // add event listners
+  .on("mouseover", function(d) {
+    d3.select(this).transition().duration(300).style("opacity", 1);
+    div2.transition().duration(300).style("opacity", 1)
+    // вывод значений из датасета в подсказку
+    var level = 'Нет данных измерений';
+    if(rateById_water[d.properties.ISO_2]<33) {
+      level = 'Низкий уровень загрязнения';
+    }
+    if(rateById_water[d.properties.ISO_2]>=33 && rateById[d.properties.ISO_2]<67) {
+      level = 'Повышенный уровень загрязнения';
+    }
+    if(rateById_water[d.properties.ISO_2]>=67) {
+      level = 'Высокий уровень загрязнения';
+    }
+    var percent = '';
+    if(rateById_water[d.properties.ISO_2]>=0) {
+      percent = " ("+rateById_water[d.properties.ISO_2]+"%)";
+    }
+    div2.html(nameById[d.properties.ISO_2] + "<br/>" + "<span style='font-size:16px;font-weight:700'>" + level + "</span>"+percent)
       .style("left", (d3.event.layerX) + "px")
       .style("top", (d3.event.layerY - 30) + "px");
   })
@@ -164,7 +266,7 @@ function ready(error, map, data) {
   // добавление городов на карту
   d3.tsv("/data/cities.tsv", function(error, data) {
     // создание массива данных о городах
-    var city = svg.selectAll("g.city")
+    var city = svg2.selectAll("g.city")
       .data(data)
       .enter()
       .append("g")
@@ -184,6 +286,18 @@ function ready(error, map, data) {
         return d.City;
       });
   });
+
+
+
+
+
+
+
+
+
+
+
+
 
 };
 
